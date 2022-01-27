023b9634d9d8c6f9705ae174323e53cda3a8c309
Shristy Gupta <89759891+shristyguptaa@users.noreply.github.com>
2022-01-27 10:12:09 +0530

    Create Convert numbers into words.py

---
+++ Convert numbers into words.py
@@ -0,0 +1,24 @@
+# WAP to convert all the numbers between 1 to 10 into words
+a = int(input("Enter a number"))
+if a == 1:
+    print("ONE")
+elif a == 2:
+    print("TWO")
+elif a == 3:
+    print("THREE")
+elif a == 4:
+    print("FOUR")
+elif a == 5:
+    print("FIVE")
+elif a == 6:
+    print("SIX")
+elif a == 7:
+    print("SEVEN")
+elif a == 8:
+    print("EIGHT")
+elif a == 9:
+    print("NINE")
+elif a == 10:
+    print("TEN")
+else:
+    print("Enter a valid number")
